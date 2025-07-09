a05debfc0e2357ebe3f05faa48867ae09f6f430f
Mar1808 <123977564+Mar1808@users.noreply.github.com>
2025-07-09 19:47:10 +0200

    Delete Xplain/xplain directory

--- Xplain/xplain/src/main/java/fr/uge/xplain/bd/ExplanationRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.uge.xplain.bd;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ExplanationRepository extends JpaRepository<Explanation, Long> {
-    List<Explanation> findByOrderByIdDesc();
-}
